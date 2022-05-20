05abdb64663808897a919fdca4cc7ea211b2494c
Giacomo Avanzi <giacomo.avanzi2@alice.it>
2022-05-20 09:56:07 +0200

    Modified MainActivity in LoginActivity

--- app/src/main/java/it/unipd/dei/esp2122/passwordmanager/LoginActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2122/passwordmanager/LoginActivity.kt
@@ -3,9 +3,9 @@ package it.unipd.dei.esp2122.passwordmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
