15adeb8e916f35233b07f27c2ef21a0233eb9557
Giacomo Avanzi <giacomo.avanzi2@alice.it>
2022-05-20 11:43:48 +0200

    Added Login and Register Activities with their layout, and raw login interface

--- app/src/main/java/it/unipd/dei/esp2122/passwordmanager/LoginActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2122/passwordmanager/LoginActivity.kt
@@ -1,11 +1,24 @@
 package it.unipd.dei.esp2122.passwordmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val preferences = getSharedPreferences("SP1", MODE_PRIVATE)
+        val master_password = preferences.getString("Master_Password", null)
+        Log.d("Tag", master_password.toString())
+        if (master_password == null){
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         setContentView(R.layout.activity_login)
+
     }
 }
--- app/src/main/java/it/unipd/dei/esp2122/passwordmanager/RegisterActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2122/passwordmanager/RegisterActivity.kt
@@ -1,4 +1,19 @@
 package it.unipd.dei.esp2122.passwordmanager
 
-class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val preferences = getSharedPreferences("SP1", MODE_PRIVATE)
+        val editor = preferences.edit()
+
+        editor.putString("Master_Password", "pippo")
+        editor.apply()
+
+
+    }
 }
