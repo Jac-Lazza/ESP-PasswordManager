29d4664dbbf615d6f89c6000642b00a8c9b84e3b
Giacomo Avanzi <giacomo.avanzi2@alice.it>
2022-05-20 13:00:43 +0200

    Completed Login and Register Activities interface

--- app/src/main/java/it/unipd/dei/esp2122/passwordmanager/LoginActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2122/passwordmanager/LoginActivity.kt
@@ -3,22 +3,38 @@ package it.unipd.dei.esp2122.passwordmanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val preferences = getSharedPreferences("SP1", MODE_PRIVATE)
-        val master_password = preferences.getString("Master_Password", null)
-        Log.d("Tag", master_password.toString())
-        if (master_password == null){
+        val preferences = getSharedPreferences(packageName, MODE_PRIVATE)
+        val masterPassword = preferences.getString(getString(R.string.KEY_MASTER_PASSWORD), null)
+
+        if (masterPassword == null){
             val intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
-
         setContentView(R.layout.activity_login)
+
+        val tv_hello : TextView = findViewById(R.id.tv_hello)
+        val btn_login : Button = findViewById(R.id.btn_login)
+        val et_login_pwd : TextView = findViewById(R.id.et_login_pwd)
+
+        val name = preferences.getString(getString(R.string.KEY_NAME), null)
+        tv_hello.text = getString(R.string.hello, name)
+
+        btn_login.setOnClickListener {
+            val login_pwd = et_login_pwd.text.toString()
+            if (login_pwd == masterPassword)
+                Toast.makeText(applicationContext, "Password Corretta", Toast.LENGTH_LONG).show()
+            else
+                Toast.makeText(applicationContext, "Password Errata", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
--- app/src/main/java/it/unipd/dei/esp2122/passwordmanager/RegisterActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2122/passwordmanager/RegisterActivity.kt
@@ -2,17 +2,38 @@ package it.unipd.dei.esp2122.passwordmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val preferences = getSharedPreferences("SP1", MODE_PRIVATE)
-        val editor = preferences.edit()
+        val et_name : EditText = findViewById(R.id.et_name)
+        val et_insert_pwd : EditText = findViewById(R.id.et_insert_pwd)
+        val et_confirm_pwd : EditText = findViewById(R.id.et_confirm_pwd)
+        val btn_register : Button = findViewById(R.id.btn_register)
 
-        editor.putString("Master_Password", "pippo")
-        editor.apply()
+        btn_register.setOnClickListener {
+            val name = et_name.text.toString()
+            val insert_pwd = et_insert_pwd.text.toString()
+            val confirm_pwd = et_confirm_pwd.text.toString()
+            //Fare funzione ausiliaria per gestire il controllo delle password
+            if (insert_pwd == confirm_pwd){
+                val preferences = getSharedPreferences(packageName, MODE_PRIVATE)
+                val editor = preferences.edit()
+                editor.putString(getString(R.string.KEY_NAME), name)
+                editor.putString(getString(R.string.KEY_MASTER_PASSWORD), insert_pwd)
+                editor.apply()
+                Toast.makeText(applicationContext, "Password Coincidenti", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(applicationContext, "Password Non Coincidenti, RIPROVA", Toast.LENGTH_LONG).show()
+            }
+
+        }
 
 
     }
